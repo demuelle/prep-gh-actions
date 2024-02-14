de0c7f8be37995dfa871ea42c27157242e79a816
demuelle <dmueller@trilogyed.com>
2024-02-14 09:54:30 -0600

    Fixes linting issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const App = 12
 function App() {
   const [count, setCount] = useState(0)
+  const appx = 12;
 
   return (
     <>
@@ -20,7 +20,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count}...  {appx}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
